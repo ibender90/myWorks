0c909fbe0d51fbb21d8d1b7fdfed242d3248e9c6
Artjom_Stalev <stalev.artjom@gmail.com>
2022-04-08 16:24:53 +0300

    homework6 with cats and dogs written in separate branch

---
+++ src/main/java/ru/geekbrains/homework6/Cat.java
@@ -0,0 +1,20 @@
+package ru.geekbrains.homework6;
+
+public class Cat extends Animal {
+    private static int catCount = 0;
+
+    protected Cat(String name) {
+        super(name);
+        setRunAgility(200);
+        catCount++;
+    }
+
+    @Override
+    protected void swim(int distance) {
+        System.out.println("Кот отказывается плыть");
+    }
+
+    public static int getCatCount() {
+        return catCount;
+    }
+}
